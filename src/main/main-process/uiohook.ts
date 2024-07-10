99080220d45ff77e9d9ec2c523149fa2830ffc5b
rmst <rmstsd@qq.com>
2024-07-10 11:49:01 +0800

    up

--- src/main/main-process/uiohook.ts
+++ src/main/main-process/uiohook.ts
@@ -1,37 +1,8 @@
-import { BrowserWindow, globalShortcut, screen } from 'electron'
-import { uIOhook, UiohookKey } from 'uiohook-napi'
+import { globalShortcut, screen } from 'electron'
 
 import { electronWindow } from './electronWindow'
 
-let prevTime = 0
-// 全局鼠标事件
-const addUiohook = () => {
-  uIOhook.on('keyup', evt => {
-    // if (evt.keycode === UiohookKey.F12) {
-    //   BrowserWindow.getFocusedWindow()?.webContents.openDevTools({ mode: 'detach' })
-    // }
-    // if (evt.keycode === UiohookKey.F5) {
-    //   BrowserWindow.getFocusedWindow()?.webContents.reloadIgnoringCache()
-    // }
-    // if (evt.keycode === UiohookKey.Escape) {
-    //   BrowserWindow.getFocusedWindow()?.hide()
-    // }
-
-    if (evt.keycode === UiohookKey.Alt) {
-      const cur = performance.now()
-      if (cur - prevTime < 300) {
-        handleNumWindow()
-      }
-
-      prevTime = cur
-    }
-  })
-
-  uIOhook.start()
-}
-
 export const addShortcut = () => {
-  // addUiohook()
   globalShortcut.register('Alt+Space', () => {
     if (electronWindow.OpenDir.isVisible()) {
       electronWindow.OpenDir.minimize()
